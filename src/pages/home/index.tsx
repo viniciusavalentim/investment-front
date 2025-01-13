7ca91acd273f5fd07f756ae9068475dac28c9492
vinicius valentim <viniciusvalentim769@gmail.com>
2025-01-12 23:16:22 -0300

    feat(app): install and configure Shadcn ui, tailwindCSS and Framer-motion

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { RotateCw } from "lucide-react";
+
+export function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen flex-col space-y-4">
+
+            <div>
+                <Button className="px-8 py-6 bg-violet-700 hover:bg-violet-800 flex items-center gap-2 group transform transition-transform duration-300 hover:scale-105">
+                    Buscar ações
+                    <RotateCw className="transform transition-transform group-hover:animate-spin360" />
+                </Button>
+            </div>
+            <div>
+                {/* Aqui vai ficar a tabela  */}
+                <div className="w-[500px] h-[300px] border border-zinc-800"></div>
+            </div>
+        </div>
+    );
+}
