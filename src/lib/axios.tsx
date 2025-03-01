0f48eb91765c415d50780fdfb2e9c76cc02c1f9e
vinicius valentim <viniciusvalentim769@gmail.com>
2025-03-01 14:44:58 -0300

    implement a edit

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configurar a instância do Axios
 export const api = axios.create({
-    baseURL: 'http://localhost:3003',
+    baseURL: 'https://stockdeploy-api.onrender.com',
 });
 
 
